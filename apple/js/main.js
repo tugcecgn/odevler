8e5ba45146c79c3c738354c9fbec615e1211f1c4
Tuğçe İçigen <tugceicigen0917@gmail.com>
2025-03-23 13:15:47 +0300

    blog sitesi güncellendi

---
+++ apple/js/main.js
@@ -0,0 +1,24 @@
+(function(){
+    'use strict'
+    var camera = new Swiper ( '#camera .swiper',{
+        speed:600,
+        spaceBetween:12,
+        navigation:{
+            enabled:true,
+            prevEl: '.swiper-button-prev',
+            nextEl:'.swiper-button-next',
+        }
+    });
+
+    var photos = GLightbox({
+        selector:'#photos .photo'
+    });
+    var comment= new Swiper('#comments .swiper',{
+        pagination:{
+            el:'.swiper-pagination',
+            clickable: true,
+            type:'bullets',
+        }
+    });
+})();
+
